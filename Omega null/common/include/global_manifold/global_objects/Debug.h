823ae998f16f24eea69586beff24f1921b9bbab4
classic-sasquatch-behavior <classic.sasquatch.behavior@gmail.com>
2022-08-13 00:18:13 -0400

    debug complete

--- Omega null/common/include/global_manifold/global_objects/Debug.h
+++ Omega null/common/include/global_manifold/global_objects/Debug.h
@@ -12,49 +12,34 @@ namespace on {
 			impossible = 4
 		};
 
-	On_Being Error{
-		Error() {
-
-		};
-		Debug::strangeness strangeness = unknown;
-		std::string comment = "no comment.";
-		std::string file = "unspecified";
-		uint line = 0;
-	};
-
-		On_Process Print {
-
+		On_Being Error {
+			Debug::strangeness strangeness;
+			std::string comment;
+			std::string file;
+			uint line;
+			Error() : comment(""), strangeness(unknown), file("unspecified"), line(0) {}
 		};
 
 		On_Process Throw {
-			static void error(Debug::Error& error) {
+			static void error(Debug::Error & error) {
+				std::string message = "ON::ERROR : ";
+				message += error.comment;
+				message += " : this happened in file : ";
+				message += error.file;
+				message += " : at line : ";
+				message += error.line;
+
 				switch (error.strangeness) {
-				case likely:
-				case plausible:
-				case unknown:
-				case unlikely:
-				case impossible:
-				default: error(*new Error(strangeness::impossible, "...what did you say to me?", __FILE__, __LINE__)); break; //let's not start that again
+					case likely: message += " : you knew that would happen [0/4]"; break;
+					case plausible: message += " : you thought that might happen [1/4]"; break;
+					case unknown: message += " : you weren't sure if that would happen [2/4]"; break;
+					case unlikely: message += " : you didn't think that would happen [3/4]"; break;
+					case impossible: message += " : that definitely wasn't supposed to happen [4/4]"; break;
+				default: message += " : ...what? [?/ 4]"; break;
 				}
-
-
-
-
+				std::cout << "\n" + message + "\n";
 			}
-		}
-
-
-
-
-
-
-
-
-
-
-
-
-
+		};
 
 	}
 }
