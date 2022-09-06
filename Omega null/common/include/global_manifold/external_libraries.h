405dd71149bd8eb001cdd1477d53eb54c52b755d
classic-sasquatch-behavior <classic.sasquatch.behavior@gmail.com>
2022-09-06 03:29:29 -0400

    changed the header lib to be called skeleton key, now everythings outdated and I get to fix it before doing the fun stuff. yay

--- Omega null/common/include/global_manifold/external_libraries.h
+++ Omega null/common/include/global_manifold/external_libraries.h
@@ -39,8 +39,8 @@ typedef pugi::xml_node Node;
 //opencv
 #include<opencv2/core.hpp>
 #include<opencv2/core/cuda.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/imgproc.hpp>
+#include<opencv2/highgui.hpp>
 
-#include<on_header_lib.h>
+#include<skeleton_key.h>
